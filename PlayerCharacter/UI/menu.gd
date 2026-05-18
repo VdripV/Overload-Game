6b8d894ea586d9cd5a9cacdf12cfc565688c5a53
Maxim <maxim@MacBook-Air-Maxim.local>
2026-05-18 23:59:00 +0200

    Added new map, 3d models, plugins

--- PlayerCharacter/UI/menu.gd
+++ PlayerCharacter/UI/menu.gd
@@ -4,7 +4,7 @@ func _input(event: InputEvent) -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Map/template_map_scene.tscn")
+	get_tree().change_scene_to_file("res://Map/Grid_level.tscn")
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://PlayerCharacter/UI/options.tscn")
